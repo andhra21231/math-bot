d9e25e43f7193c74265687d58b38b8b319b31221
andhra21231 <69280997+andhra21231@users.noreply.github.com>
2021-05-06 07:52:50 +0700

    elif change to if

--- core_script/+_-_x.py
+++ core_script/+_-_x.py
@@ -32,11 +32,11 @@ while True:
         if choice == '1':
             print(num1, "Ditambah", num2, "Hasilnya Adalah", add(num1, num2))
 
-        elif choice == '2':
+        if choice == '2':
             print(num1, "Dikurangi", num2, "Hasilnya Adalah", subtract(num1, num2))
 
-        elif choice == '3':
+        if choice == '3':
             print(num1, "Dikali", num2, "Hasilnya Adalah", multiply(num1, num2))
 
-        elif choice == '4':
+        if choice == '4':
             print(num1, "Dibagi", num2, "Hasilnya Adalah", divide(num1, num2))
