b67c21f24d5b0b762f96c915351cc854fbd03296
CoolNuttle <73450047+CoolNuttle@users.noreply.github.com>
2021-05-06 09:26:49 +0700

    Update volume.py

--- core_script/volume.py
+++ core_script/volume.py
@@ -6,39 +6,42 @@ def balok(x, y, z):
     return x * y * z
 
 
-def prism(x, y):
-    return x * y
+def tri_prism_alas(x, y):
+    return x * y / 2
+
+def tri_prism_hasil(x, y, z):
+    return tri_prism_alas(x, y) * z
 
 
 def silinder(x, y):
-    return (3.14 * x * x) * y
+    return 22/7 * x * x * y
 
 
 def silinder_2(x, y):
-    return (22 / 7 * x * x) * y
+    return 3.14 * x * x * y
 
 
 def pyramid(x, y):
-    return (1 / 3 * x) * y
+    return 1/3 * x * y
 
 
 def sphere(x):
-    return 4 / 3 * 3.14 * x * x * x
+    return 4/3 * 3.14 * x * x * x
 
 
 def sphere_b(x):
-    return 4 / 3 * 22 / 7 * x * x * x
+    return 4/3 * 22/7 * x * x * x
 
 
 print("Apa Yang Ingin Kamu Kalkulasi ? ")
 print("1. Kotak")
 print("2. Balok")
-print("3. Prisma")
-print("4. Silinder Dengan Pi : 22/7 (Kelipatan 7)")
-print("5. silinder Dengan Pi : 3.14")
+print("3. Prisma Segitiga")
+print("4. Silinder Dengan Phi : 22/7 (Kelipatan 7)")
+print("5. silinder Dengan Phi : 3.14")
 print("6. Piramida")
-print("7. bola Dengan Pi : 22/7 (Kelipatan 7)")
-print("8. bola Dengan Pi : 3.14")
+print("7. bola Dengan Phi : 22/7 (Kelipatan 7)")
+print("8. bola Dengan Phi : 3.14")
 
 
 while True:
@@ -59,11 +62,13 @@ while True:
         print(num1, "Dikali", num2, "Dikali", num3, "Sama Dengan", balok(num1, num2, num3))
 
     if choice in ('3'):
-        num1 = float(input("Masukan Dasar Prisma :"))
-        num2 = float(input("Masukan Tinggi Prisma :"))
+        num1 = float(input("Masukan Panjang Alas :"))
+        num2 = float(input("Masukan Tinggi Alas :"))
+        num3 = float(input("Masukan Tinggi Prisma :"))
 
     if choice == '3':
-        print(num1, "Dikali", num2, "Sama Dengan",prism(num1, num2))
+        print("Luas alas =", num1, "dikali", num2, "dibagi 2 hasilnya", tri_prism_alas(num1, num2))
+        print("Volume prisma =", tri_prism_hasil(num1, num2, num3))
 
     if choice in ('4'):
         num1 = float(input("masukan Radius Silinder (Bisa Didapat Di Area Calculation) : "))
@@ -81,7 +86,7 @@ while True:
 
     if choice in ('6'):
         num1 = float(input("masukan dasar dari piramidanyanya : "))
-        num2 = float(input("masukan tinggi dari primadinya : "))
+        num2 = float(input("masukan tinggi dari piramidnya : "))
 
     if choice == '6':
         print("1/3 dikalikan", num1, "dikali",num2, "sama dengan", pyramid(num1, num2))
